51e14aabba339d2e4714c3ca174f1f07b49e7995
tobi <noggnzzz@gmail.com>
2023-12-13 17:34:45 -0500

    leyenda y cambio de shape a poligono por cada predio

--- js/data/logic_layers.js
+++ js/data/logic_layers.js
@@ -14,24 +14,40 @@ var map = L.map('map', {
 //Escogemos el mapabase que usaremos (añadir más)
 L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}',{ }).addTo(map);
 
-// Botón de restablecer vista del mapa
-var resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', function() {
-    map.setView([(boundary[1] + boundary[3]) / 2, (boundary[0] + boundary[2]) / 2], zoomLevel);
-});
+
 
 /**
  * lÓGICA DEL MANEJO DE CAPAS EN EL MAPA
  */
 
 var layerAlto = L.geoJSON(alto, {
+  
   style: function (feature) {
       return {
           color: "#ff0000",
           weight: 1,
           opacity: 0.95
       };
+  },
+
+  onEachFeature: function (feature, layer) {
+    // Construir el contenido del popup utilizando propiedades específicas
+    var popupContent =  "<div class='pop'>"+
+      "<span><b>Nombre Local:</b><p>" + feature.properties.nom_local + "</p></span>" +
+      "<span><b>Urbanización:</b><p>" + feature.properties.Urbanizaci + "</p></span>" +
+      "<span><b>Tipo de Vía:</b><p>" + feature.properties.Tipo_de_vi + "</p></span>" +
+      "<span><b>Material:</b><p>" + feature.properties.Material_p + "</p></span>" +
+      "<span><b>Número de Pisos:</b><p>" + feature.properties.num_pisos + "</p></span>" +
+      "<span><b>Distrito:</b><p>" + feature.properties.Distrito + "</p></span>" +
+      "<span><b>Grado de Afectación:</b><p>" + feature.properties.Grado_afec + "</p></span>" +
+      "<span><b>Tipo de Edificio:</b><p>" + feature.properties.Tipo_edifi + "</p></span>" +
+      +"</div>"
+
+    // Vincular el popup al layer
+    layer.bindPopup(popupContent);
   }
+
+  
 }).addTo(map);
 
 var layerModerado = L.geoJSON(moderado, {
@@ -41,7 +57,26 @@ var layerModerado = L.geoJSON(moderado, {
           weight: 1,
           opacity: 0.95
       };
+  },
+
+  onEachFeature: function (feature, layer) {
+    // Construir el contenido del popup utilizando propiedades específicas
+    var popupContent =  "<div class='pop'>"+
+      "<span><b>Nombre Local:</b><p>" + feature.properties.nom_local + "</p></span>" +
+      "<span><b>Urbanización:</b><p>" + feature.properties.Urbanizaci + "</p></span>" +
+      "<span><b>Tipo de Vía:</b><p>" + feature.properties.Tipo_de_vi + "</p></span>" +
+      "<span><b>Material:</b><p>" + feature.properties.Material_p + "</p></span>" +
+      "<span><b>Número de Pisos:</b><p>" + feature.properties.num_pisos + "</p></span>" +
+      "<span><b>Distrito:</b><p>" + feature.properties.Distrito + "</p></span>" +
+      "<span><b>Grado de Afectación:</b><p>" + feature.properties.Grado_afec + "</p></span>" +
+      "<span><b>Tipo de Edificio:</b><p>" + feature.properties.Tipo_edifi + "</p></span>" +
+      +"</div>"
+
+    // Vincular el popup al layer
+    layer.bindPopup(popupContent);
   }
+
+
 }).addTo(map);
 
 var layerNinguno = L.geoJSON(ninguno, {
@@ -51,35 +86,53 @@ var layerNinguno = L.geoJSON(ninguno, {
           weight: 1,
           opacity: 0.95
       };
+  },
+
+  onEachFeature: function (feature, layer) {
+    // Construir el contenido del popup utilizando propiedades específicas
+    var popupContent =  "<div class='pop'>"+
+      "<span><b>Nombre Local:</b><p>" + feature.properties.nom_local + "</p></span>" +
+      "<span><b>Urbanización:</b><p>" + feature.properties.Urbanizaci + "</p></span>" +
+      "<span><b>Tipo de Vía:</b><p>" + feature.properties.Tipo_de_vi + "</p></span>" +
+      "<span><b>Material:</b><p>" + feature.properties.Material_p + "</p></span>" +
+      "<span><b>Número de Pisos:</b><p>" + feature.properties.num_pisos + "</p></span>" +
+      "<span><b>Distrito:</b><p>" + feature.properties.Distrito + "</p></span>" +
+      "<span><b>Grado de Afectación:</b><p>" + feature.properties.Grado_afec + "</p></span>" +
+      "<span><b>Tipo de Edificio:</b><p>" + feature.properties.Tipo_edifi + "</p></span>" +
+      +"</div>"
+
+    // Vincular el popup al layer
+    layer.bindPopup(popupContent);
   }
+
 }).addTo(map);
 
-var anotherLayer = L.geoJSON(data, {
-  pointToLayer: function (feature, latlng) {
-      // Utilizar el marcador predeterminado de Leaflet (un simple círculo)
-      var marker = L.circleMarker(latlng, { 
-          radius: 6,  // Ajusta el tamaño del círculo según tus necesidades
-          fillColor: "skyblue",  // Ajusta el color del círculo según tus necesidades
-          color: "#000",  // Ajusta el borde del círculo según tus necesidades
-          weight: 1,  // Ajusta el grosor del borde según tus necesidades
-          opacity: 1,  // Ajusta la opacidad según tus necesidades
-          fillOpacity: 0.7  // Ajusta la opacidad del relleno según tus necesidades
-      });
+// var anotherLayer = L.geoJSON(data, {
+//   pointToLayer: function (feature, latlng) {
+//       // Utilizar el marcador predeterminado de Leaflet (un simple círculo)
+//       var marker = L.circleMarker(latlng, { 
+//           radius: 6,  // Ajusta el tamaño del círculo según tus necesidades
+//           fillColor: "skyblue",  // Ajusta el color del círculo según tus necesidades
+//           color: "#000",  // Ajusta el borde del círculo según tus necesidades
+//           weight: 1,  // Ajusta el grosor del borde según tus necesidades
+//           opacity: 1,  // Ajusta la opacidad según tus necesidades
+//           fillOpacity: 0.7  // Ajusta la opacidad del relleno según tus necesidades
+//       });
       
-      // Agregar información al popup
-      if (feature.properties) {
-        var popupContent =  "<div class='pop'>"+
-                              "<span><b>Urbanización:</b><p>"+feature.properties.Urbanizaci+"</p></span>"+
-                              "<span><b>Tipo de vía:</b><p>"+feature.properties.Tipo_de_vi+"</p></span>"+
-                              "<span><b>Material de pavimento:</b><p>"+feature.properties.Material_p+"</p></span>"+
-                              "<span><b>Número de pisos:</b><p>"+feature.properties.num_pisos+"</p></span>"+
-                              "<span><b>Distrito:</b><p>"+feature.properties.Distrito+"</p></span>"+
-                            +"</div>"
+//       // Agregar información al popup
+//       if (feature.properties) {
+//         var popupContent =  "<div class='pop'>"+
+//                               "<span><b>Urbanización:</b><p>"+feature.properties.Urbanizaci+"</p></span>"+
+//                               "<span><b>Tipo de vía:</b><p>"+feature.properties.Tipo_de_vi+"</p></span>"+
+//                               "<span><b>Material de pavimento:</b><p>"+feature.properties.Material_p+"</p></span>"+
+//                               "<span><b>Número de pisos:</b><p>"+feature.properties.num_pisos+"</p></span>"+
+//                               "<span><b>Distrito:</b><p>"+feature.properties.Distrito+"</p></span>"+
+//                             +"</div>"
 
-        marker.bindPopup(popupContent);
-    }
+//         marker.bindPopup(popupContent);
+//     }
 
-    return marker;
+//     return marker;
       
-  }
-}).addTo(map);
+//   }
+// }).addTo(map);
